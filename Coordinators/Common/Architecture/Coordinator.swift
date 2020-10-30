151378f2b3bcd042059bea493a30d5b11333bd65
Maciej Górecki <maciekgorecki@icloud.com>
2020-10-30 22:11:11 +0100

    Added first coordination

---
+++ Coordinators/Common/Architecture/Coordinator.swift
@@ -0,0 +1,8 @@
+//
+//  Coordinator.swift
+//  Coordinators
+//
+//  Created by Maciej Gorecki on 30/10/2020.
+//
+
+import Foundation
---
+++ Coordinators/Features/Authentication/LoginViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  LoginViewModel.swift
+//  Coordinators
+//
+//  Created by Maciej Gorecki on 30/10/2020.
+//
+
+import Foundation
