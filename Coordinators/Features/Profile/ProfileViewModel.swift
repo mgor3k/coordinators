5bd25f513d98a261ea95cf7aa36c929d7e528f21
Maciej Górecki <maciekgorecki@icloud.com>
2020-11-08 13:31:25 +0100

    Removed unused code

--- Coordinators/Features/Profile/ProfileViewModel.swift
+++ Coordinators/Features/Profile/ProfileViewModel.swift
@@ -4,18 +4,12 @@
 
 import Foundation
 
-protocol ProfileDelegate: class {
-    func didClose()
-}
+protocol ProfileDelegate: class {}
 
 class ProfileViewModel {
     private weak var delegate: ProfileDelegate?
     
     init(delegate: ProfileDelegate) {
         self.delegate = delegate
-    }
-    
-    func close() {
-        delegate?.didClose()
     }
 }
