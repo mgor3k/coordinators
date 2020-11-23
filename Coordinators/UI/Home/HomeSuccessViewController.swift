8b743ba61521398769b3f5f940c6702323899932
Maciej Górecki <maciekgorecki@icloud.com>
2020-11-23 20:40:23 +0100

    Added lottie button.

--- Coordinators/UI/Home/HomeSuccessViewController.swift
+++ Coordinators/UI/Home/HomeSuccessViewController.swift
@@ -3,14 +3,24 @@
 //
 
 import UIKit
+import Lottie
 
 class HomeSuccessViewController: ViewController {
+    private let containerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .white
+        view.layer.cornerRadius = 20
+        return view
+    }()
+    
     private let titleLabel: UILabel = {
         let label = UILabel()
-        label.textColor = .white
+        label.textColor = .black
         return label
     }()
-        
+    
+    private let button = AnimatedButton(animation: Animation.named("heart")!)
+    
     init(name: String) {
         super.init()
         
@@ -20,14 +30,54 @@ class HomeSuccessViewController: ViewController {
     override func setup() {
         view.backgroundColor = .purple
         setupLayout()
+        setupAnimationButton()
     }
 }
 
 private extension HomeSuccessViewController {
     func setupLayout() {
-        view.addSubview(titleLabel)
+        view.addSubview(containerView)
+        containerView.addSubview(titleLabel)
+        containerView.addSubview(button)
+        
+        containerView.snp.makeConstraints {
+            $0.center.equalToSuperview()
+            $0.size.equalTo(CGSize(width: 250, height: 250))
+        }
+        
         titleLabel.snp.makeConstraints {
             $0.center.equalToSuperview()
         }
+        
+        button.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.bottom).offset(16)
+            $0.centerX.equalToSuperview()
+        }
+    }
+    
+    func setupAnimationButton() {
+        button.setPlayRange(
+            fromMarker: "touchDownStart",
+            toMarker: "touchDownEnd",
+            event: .touchDown
+        )
+        
+        button.setPlayRange(
+            fromMarker: "touchDownEnd",
+            toMarker: "touchUpCancel",
+            event: .touchUpOutside
+        )
+
+        button.setPlayRange(
+            fromMarker: "touchDownEnd",
+            toMarker: "touchUpEnd",
+            event: .touchUpInside
+        )
+        
+        button.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+    }
+    
+    @objc func buttonTapped() {
+        button.isEnabled = false
     }
 }
